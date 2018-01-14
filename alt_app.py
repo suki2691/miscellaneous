6f4b1be7f5b0cffba52f5a5f0ff08b1834a0685c
suki2691 <v.sukeerthi@gmail.com>
2018-01-14 00:56:18 -0800

    updates with mongoengine and params

--- alt_app.py
+++ alt_app.py
@@ -4,35 +4,38 @@ import json
 import pandas as pd
 from datetime import datetime
 import re
-from wsgiref import simple_server  
+from wsgiref import simple_server
+import mongoengine as mongo
+from mongo_data import User, Experience, Education, CareerTimeline
 
-client = MongoClient('localhost',27017)
-db = client['user_data']
-user = db.posts
+mongo.connect('user_data',port=27017)
 
-def get_work_experience(user):
-    first_name = input('Enter first name: ')
+def get_work_experience(first_name):
+    # first_name = input('Enter first name: ')
     output_dict_list = []
     output_dict = {}
-    for i in list(user.find({'FirstName':first_name})):
-        output_dict_list.append({'FirstName': first_name, 'LastName':i['LastName'], 'FirstWorkExperience':i['CareerTimeline']['WorkExperience'][-1]['title']})
+    for i in User.objects(FirstName=first_name):
+        output_dict_list.append({'FirstName': i.FirstName, 'LastName':i.LastName, 'FirstWorkExperience':i.CareerTimelines.WorkExperience[-1].Title})
     if output_dict_list == []:
         return None
     else:
         return json.dumps(output_dict_list)
 
 class ThingsResource(object):
+
     def on_get(self, req, resp):
         """Handles GET requests"""
-        resp.body = get_work_experience(user)
+        first_name = req.params['firstname']
+        resp.body = get_work_experience(first_name)
         if resp.body == None:
             resp.status = falcon.HTTP_404
         else:
             resp.status = falcon.HTTP_200
-        
+
 # falcon.API instances are callable WSGI apps
 app = falcon.API()
-
+app = api = falcon.API()
+app.req_options.auto_parse_form_urlencoded = True
 # Resources are represented by long-lived class instances
 things = ThingsResource()
 
--- mongo_data.py
+++ mongo_data.py
@@ -3,66 +3,118 @@ from pymongo import MongoClient
 import json
 import pandas as pd
 from datetime import datetime
-import re  
+import re
+import mongoengine as mongo
+import dateparser
 
-def experience(parsed):
-    new_data = parsed['experience']
-    experience_data ={k.replace('.','').replace('/',' '):v for k,v in new_data.items()}
-#     print(experience_data)
-    k = 0
-    exps = []
-    for i in experience_data.keys():
-        exp = {}
-        exp['id'] = k
-        exp['title'] = i
-        if experience_data[i]:
-            if ' to ' in experience_data[i]:
-                exp['startdate'] = experience_data[i].split(' to ')[0]
-                exp['enddate'] = experience_data[i].split(' to ')[1]
-            elif ' a ' in experience_data[i]:
-                exp['startdate'] = experience_data[i].split(' a ')[0]
-                exp['enddate'] = experience_data[i].split(' a ')[1]
-            elif ' bis ' in experience_data[i]:
-                exp['startdate'] = experience_data[i].split(' bis ')[0]
-                exp['enddate'] = experience_data[i].split(' bis ')[1]
-        else:
-            exp['startdate'] = 'None'
-            exp['enddate'] = 'None'
-        k += 1
-        exps.append(exp)
-    return exps
+# creating Mongo Engine models
+class Experience(mongo.EmbeddedDocument):
+    Id = mongo.ObjectIdField()
+    Title = mongo.StringField()
+    StartDate = mongo.DateTimeField()
+    EndDate = mongo.DateTimeField()
 
-def career_timeline(parsed,id):
-    career = {}
-    career['id'] = id
-    career['WorkExperience'] = experience(parsed)
-    if 'education' in parsed.keys():
-        new_parsed = {k.replace(".", ""):v for k,v in parsed['education'].items()}
-        career['EducationExperience'] = new_parsed
-    else:
-        career['EducationExperience'] = {}
-    career['Location'] = parsed['location']
-    return career
+class Education(mongo.EmbeddedDocument):
+    Degree = mongo.StringField()
+    StartDate = mongo.DateTimeField()
+    EndDate = mongo.DateTimeField()
 
-def users(data):
-    user = {}
-    page = open("CareerTimelines/"+data['CareerTimeline'], 'r')
-    parsed = json.loads(page.read())
-    user['id'] = data['Id']
-    user['FirstName'] = data['First Name']
-    user['LastName'] = data['Last Name']
-    user['Email'] = data['Email']
-    user['CareerTimeline'] = career_timeline(parsed,data['Id'])
-    return user
+class CareerTimeline(mongo.Document):
+    Id = mongo.ObjectIdField()
+    WorkExperience = mongo.ListField(mongo.EmbeddedDocumentField('Experience'))
+    EducationExperience = mongo.ListField(mongo.EmbeddedDocumentField('Education'))
+    Location = mongo.StringField()
 
+class User(mongo.Document):
+    Id = mongo.ObjectIdField()
+    FirstName = mongo.StringField(required=True)
+    LastName = mongo.StringField(required=True)
+    Email = mongo.StringField(required=True)
+    CareerTimelines = mongo.ReferenceField(CareerTimeline)
+
+#Extracting data from excel sheet data in MongoDB
 excel_data = pd.read_excel('Database.xlsx').reset_index()
 excel_data.columns = ['Id', 'First Name', 'Last Name', 'Email', 'CareerTimeline']
 user_dict = excel_data.to_dict('records')
 
+#Creating MongoDB
 client = MongoClient('localhost', 27017)
+
 client.test_database
+
 db = client['user_data']
+
 user_collection = db['users']
-user = db.posts
-for i in user_dict:
-    user.insert_one(users(i))
+
+post = db.posts
+
+if __name__ == '__main__':
+    #Service route to format data to feed into MongoDB
+    def load_user_data(parsed):
+    #     print(parsed)
+        new_data = parsed['experience']
+        experience_data ={k.replace('.','').replace('/',' '):v for k,v in new_data.items()}
+
+        exp = []
+        if parsed['experience']:
+            for i in experience_data.keys():
+
+                experience = Experience(Title = i)
+                if experience_data[i]:
+                    if ' to ' in experience_data[i]:
+                        experience.StartDate = dateparser.parse(experience_data[i].split(' to ')[0])
+                        experience.EndDate = dateparser.parse(experience_data[i].split(' to ')[1])
+                    elif ' a ' in experience_data[i]:
+                        experience.StartDate = dateparser.parse(experience_data[i].split(' a ')[0])
+                        experience.EndDate = dateparser.parse(experience_data[i].split(' a ')[1])
+                    elif ' bis ' in experience_data[i]:
+                        experience.StartDate = dateparser.parse(experience_data[i].split(' bis ')[0])
+                        experience.EndDate = dateparser.parse(experience_data[i].split(' bis ')[1])
+                else:
+                    experience.StartDate = None
+                    experience.EndDate = None
+                exp.append(experience)
+        else:
+            experience = Experience()
+
+        ed = []
+        if 'education' in parsed.keys():
+            if parsed['education']:
+                new_parsed = {k.replace(".", ""):v for k,v in parsed['education'].items()}
+                for i in new_parsed.keys():
+                    education = Education(Degree=i)
+                    if new_parsed[i]:
+                        if ' to ' in new_parsed[i]:
+                            education.StartDate = dateparser.parse(new_parsed[i].split(' to ')[0])
+                            education.EndDate = dateparser.parse(new_parsed[i].split(' to ')[1])
+                        elif ' a ' in new_parsed[i]:
+                            education.StartDate = dateparser.parse(new_parsed[i].split(' a ')[0])
+                            education.EndDate = dateparser.parse(new_parsed[i].split(' a ')[1])
+                        elif ' bis ' in new_parsed[i]:
+                            education.StartDate = dateparser.parse(new_parsed[i].split(' bis ')[0])
+                            education.EndDate = dateparser.parse(new_parsed[i].split(' bis ')[1])
+                        else:
+                            education.StartDate = None
+                            education.EndDate = None
+                    ed.append(education)
+
+            else:
+                education = Education()
+
+        else:
+            education = Education()
+
+        careerTimeline = CareerTimeline(WorkExperience=exp,EducationExperience=ed,Location=parsed['location'])
+        careerTimeline.save(cascade=True)
+
+        return careerTimeline
+
+    for i in user_dict:
+        mongo.connect('user_data',port=27017)
+        page = open("CareerTimelines/"+i['CareerTimeline'], 'r')
+        parsed = json.loads(page.read())
+        careerTimeline = load_user_data(parsed)
+        user = User(FirstName=i['First Name'],CareerTimelines=careerTimeline)
+        user.LastName = i['Last Name']
+        user.Email = i['Email']
+        user.save()
